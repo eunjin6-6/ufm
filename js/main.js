735035711846c0e63d17de6e05e78107a4b225e2
eunjin6-6 <dmswls616@gmail.com>
2024-08-21 15:38:07 +0900

    slide 자동페이드인아웃 스크립트 추가

--- js/main.js
+++ js/main.js
@@ -2,10 +2,28 @@ let menu = document.querySelector('.main_menu');
 let header = document.querySelector('.top_nav');
 const body = document.body;
 let lastScroll = 0;
+
+const slideWrapper = document.querySelector('.slide-wrapper');
+const slides = document.querySelectorAll('.slide-container li');
+const slideCount = slides.length;
+const slideDesc = document.querySelector('.slide_desc');
+let currentIdx = 0; //현재 보고 있는 인덱스번호
+let autoFade;
+const pager = slideWrapper.querySelector('.slide_pager');
+let pagerHTML = '';
+
+
 let partnerSec = document.querySelector('.partners');
 let partnerList = document.querySelector('.partner_list');
 const partners = document.querySelectorAll(".partners ul");
 
+
+
+
+
+//----------------------------------------------------------------------- 
+//header 상단 메뉴 스크립트
+//----------------------------------------------------------------------- 
 //상단 메뉴 마우스 오버시 서브메뉴펼쳐지게
 //상단 메뉴 마우스 오버시 배경 흰색, 글자 검정색
 menu.addEventListener('mouseover',function(){
@@ -19,7 +37,7 @@ menu.addEventListener('mouseout',function(){
 });
 
 
-//스크롤시 active 추가
+//마우스 스크롤시 active 추가
 window.addEventListener('scroll',()=>{
   if(window.scrollY > 0){
     header.classList.add('active');
@@ -28,7 +46,7 @@ window.addEventListener('scroll',()=>{
   }
 });
 
-//위로 올리면 없어지게
+//마우스 스크롤 위로 올리면 없어지게
 window.addEventListener('scroll',()=>{
   let currentScroll = window.scrollY;
 
@@ -44,7 +62,96 @@ window.addEventListener('scroll',()=>{
 
 
 
+//----------------------------------------------------------------------- 
+//slide 배너 이미지 자동 페이드인아웃
+//----------------------------------------------------------------------- 
+//자동 슬라이드 함수 생성
+function showSlide(num){
+
+  if(currentIdx == num) return; //보고있는 페이저 번호 누르면 아무것도 없이 바로 실행
+  let currentSlide = slides[currentIdx];
+  let nextSlide = slides[num];
+
+  //지금 슬라이드에 active 제거, 다음 슬라이드에 active 추가
+    currentSlide.classList.remove('active');
+    nextSlide.classList.add('active');
+    
+    currentIdx = num;
+    updatePager();
+}
+showSlide(0);
+
+
+
+//자동 슬라이드 실행 함수
+function autoFadeStart(){
+  autoFade = setInterval(function(){ 
+    let nextIdx = (currentIdx + 1)% slideCount; //3로 나눈 나머지
+    showSlide(nextIdx);
+  }, 2000);
+}
+//위에는 함수 할 일 정의, 열자마자 함수 실행
+autoFadeStart();
+
+
+
+//마우스가 슬라이드 텍스트에 들어오면 멈추기
+slideDesc.addEventListener('mouseenter',()=>{
+  clearInterval(autoFade);
+});
+slideDesc.addEventListener('mouseleave',()=>{
+  autoFadeStart(); //함수로 가져오게 > 통째로 함수 안에 넣어버림
+});
+
+//마우스가 페이져에 들어오면 멈추기
+pager.addEventListener('mouseenter',()=>{
+  clearInterval(autoFade);
+});
+pager.addEventListener('mouseleave',()=>{
+  autoFadeStart(); //함수로 가져오게 > 통째로 함수 안에 넣어버림
+});
+
+
+
+//----------------------------------------------------------------------- 
+//슬라이드 페이져
+//----------------------------------------------------------------------- 
+//pager 생성
+for(let i = 0;i < slideCount; i++){
+  pagerHTML += `<a href = "">${i}</a>`;
+}
+
+pager.innerHTML = pagerHTML;
+
+const pagerBtn = pager.querySelectorAll('a');
+
+
+//pager 클릭시 해당 슬라이드로 이동
+pagerBtn.forEach((pager,idx)=>{
+  pager.addEventListener('click',(e)=>{
+    e.preventDefault();
+    showSlide(idx);
+  })
+});
+
+
+
+//페이져 누르면 active 작동하게 하는 함수
+function updatePager(){
+  for(let pager of pagerBtn){
+    pager.classList.remove('active');
+  }
+  pagerBtn[currentIdx].classList.add('active');
+}
+
+updatePager(); //열자마자 실행해
+
+
+
+
+//----------------------------------------------------------------------- 
 //performance 스크롤 넘버 카운팅 이벤트
+//----------------------------------------------------------------------- 
 let numbersWrap = document.querySelector('.performance');
 let numbersOST = numbersWrap.offsetTop - 500;
 let animated = false;
@@ -74,7 +181,13 @@ window.addEventListener('scroll',()=>{
   }
 });
 
+
+
+
+
+//----------------------------------------------------------------------- 
 //파트너스 로고 무한 슬라이드
+//----------------------------------------------------------------------- 
 const copy = document.querySelector(".partners_list").cloneNode(true);
 partnerSec.appendChild(copy);
 
@@ -129,5 +242,4 @@ partnerSec.addEventListener('mouseleave',()=>{
    list.animate(keyframes, options);
  }
 */
-
 
